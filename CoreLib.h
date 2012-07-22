dfd9bf0cf5a94f3175e682ac990ecea5cea386de
Julian Mayer <julian@corecode.at>
2012-07-22 20:55:36 +0200

    updates including new custom types for collection classes that so that the compiler knows what type they return

--- CoreLib.h
+++ CoreLib.h
@@ -25,19 +25,7 @@ typedef void (^IntInBlock)(int input);
 
 #define $appbundleid		([[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleIdentifier"])
 #define $appname			([[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleName"])
-#define $earray             ([NSArray array])
-#define $emarray            ([NSMutableArray array])
-#define $array(OBJS...)     ((NSArray *)([NSArray arrayWithObjects:OBJS, nil]))
-#define $marray(OBJS...)    ([NSMutableArray arrayWithObjects:OBJS, nil])
-#define $dict(PAIRS...)     ([NSDictionary dictionaryWithObjectsAndKeys:PAIRS, nil])
-#define $mdict(PAIRS...)    ([NSMutableDictionary dictionaryWithObjectsAndKeys:PAIRS, nil])
-#define $num(num)			([NSNumber numberWithFloat:num])
-#define $numd(num)			([NSNumber numberWithDouble:num])
-#define $numi(num)			([NSNumber numberWithInteger:num])
-#define $numui(num)			([NSNumber numberWithUnsignedInt:num])
-#define $string(str...)     ([NSString stringWithString:str])
 #define $stringcu8(x)       ([NSString stringWithUTF8String:x])
-#define $mstring(str)       ([NSMutableString stringWithString:str])
 #define $stringf(format...) ([NSString stringWithFormat:format])
 #define $mstringf(format...)([NSMutableString stringWithFormat:format])
 #define $default(key)       ([[NSUserDefaults standardUserDefaults] objectForKey:key])
@@ -55,7 +43,42 @@ typedef void (^IntInBlock)(int input);
 #define $predf(format...)   ([NSPredicate predicateWithFormat:format])
 #define $alert(format...)   ({[NSApp activateIgnoringOtherApps:YES]; NSRunAlertPanel(format);})
 
+#define $md(dict)			([NSMutableDictionary dictionaryWithDictionary:dict])
+#define $ma(array)			([NSMutableArray arrayWithArray:array])
+#define $ms(str)      		([NSMutableString stringWithString:str])
+
+// made obsolete by obj-c literals in xcode 4.4
+#define $earray             ([NSArray array])
+#define $emarray            ([NSMutableArray array])
+#define $array(OBJS...)     ((NSArray *)([NSArray arrayWithObjects:OBJS, nil]))
+#define $marray(OBJS...)    ([NSMutableArray arrayWithObjects:OBJS, nil])
+#define $dict(PAIRS...)     ([NSDictionary dictionaryWithObjectsAndKeys:PAIRS, nil])
+#define $mdict(PAIRS...)    ([NSMutableDictionary dictionaryWithObjectsAndKeys:PAIRS, nil])
+#define $num(num)			([NSNumber numberWithFloat:num])
+#define $numd(num)			([NSNumber numberWithDouble:num])
+#define $numi(num)			([NSNumber numberWithInteger:num])
+#define $numui(num)			([NSNumber numberWithUnsignedInt:num])
+
 #endif
+
+// lets you define custom types for collection classes that so that the compiler knows what type they return
+#define CUSTOM_ARRAY(classname) \
+@interface classname ## Array : NSArray \
+- (classname *)objectAtIndexedSubscript:(NSUInteger)index;\
+@end
+#define CUSTOM_MUTABLE_ARRAY(classname) \
+@interface Mutable ## classname ## Array : NSMutableArray \
+- (classname *)objectAtIndexedSubscript:(NSUInteger)index;\
+@end
+#define CUSTOM_DICTIONARY(classname) \
+@interface classname ## Dictionary : NSDictionary \
+- (classname *)objectForKeyedSubscript:(id)key;\
+@end
+#define CUSTOM_MUTABLE_DICTIONARY(classname) \
+@interface Mutable ## classname ## Dictionary : NSMutableDictionary \
+- (classname *)objectForKeyedSubscript:(id)key;\
+@end
+
 
 
 #include <asl.h>
