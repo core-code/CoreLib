1027d8c95804aa7c7d68e044a92b0a84683bd300
Julian Mayer <julian@corecode.at>
2013-04-29 13:52:18 +0200

    new timer class to work around bug in NSTimer when the mac sleeps

--- iOS/JMWebViewController.h
+++ iOS/JMWebViewController.h
@@ -2,7 +2,7 @@
 //  JMWebViewController.h
 //  CoreLib
 //
-//  Created by Julian Mayer on 04.03.13.
+//  Created by CoreCode on 04.03.13.
 /*	Copyright (c) 2013 CoreCode
  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitationthe rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
