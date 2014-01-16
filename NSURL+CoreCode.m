79ccaadde3a57a3a9ae29c1b27e770716de6b5bd
Julian Mayer <julian@corecode.at>
2014-01-16 12:20:51 +0100

    work

--- NSURL+CoreCode.m
+++ NSURL+CoreCode.m
@@ -3,7 +3,7 @@
 //  CoreLib
 //
 //  Created by CoreCode on 17.12.12.
-/*	Copyright (c) 2012 - 2013 CoreCode
+/*	Copyright (c) 2012 - 2014 CoreCode
  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitationthe rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
@@ -14,7 +14,12 @@
 
 @implementation NSURL (CoreCode)
 
-@dynamic dirContents, dirContentsRecursive, fileExists, uniqueFile, path;
+@dynamic dirContents, dirContentsRecursive, fileExists, uniqueFile, path, request, fileSize;
+
+- (NSURLRequest *)request
+{
+	return [NSURLRequest requestWithURL:self];
+}
 
 - (NSURL *)add:(NSString *)component
 {
@@ -42,6 +47,16 @@
 - (BOOL)fileExists
 {
 	return [self isFileURL] && [[NSFileManager defaultManager] fileExistsAtPath:[self path]];
+}
+
+- (unsigned long long)fileSize
+{
+	NSNumber *size;
+
+	if ([self getResourceValue:&size forKey:NSURLFileSizeKey error:nil])
+        return [size unsignedLongLongValue];
+	else
+		return 0;
 }
 
 - (void)open
