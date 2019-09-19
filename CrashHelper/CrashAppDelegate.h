a55d29846560bba1b436e1e92a3a08f705a80eda
Julian Mayer <julian@corecode.io>
2019-09-19 16:56:12 +0200

    its 2019!

--- CrashHelper/CrashAppDelegate.h
+++ CrashHelper/CrashAppDelegate.h
@@ -3,7 +3,7 @@
 //  CrashHelper
 //
 //  Created by CoreCode on 23/11/16.
-//  Copyright © 2018 CoreCode Limited. All rights reserved.
+//  Copyright © 2019 CoreCode Limited. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
