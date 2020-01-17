0b19289c7fd90a0f46ae1289a08ec87b4447eec7
Julian Mayer <julian@corecode.io>
2020-01-17 15:12:12 +0100

    fix last commit

--- Mac/JMCrashReporter.m
+++ Mac/JMCrashReporter.m
@@ -97,7 +97,7 @@ void CheckAndReportCrashes(NSString *email, NSArray *neccessaryStrings)
                             foundNeccessaryString = TRUE;
                 }
 
-                if ([crashlog rangeOfString:@"gpusGenerateCrashLog"] != NSNotFound) // ignore GPU driver crashes
+                if ([crashlog rangeOfString:@"gpusGenerateCrashLog"].location != NSNotFound) // ignore GPU driver crashes
                     return;
 
                 if ([crashlog rangeOfString:@"Crashed Thread"].location == NSNotFound)
