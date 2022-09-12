eb33914544c46f3d282615b2ba5df1d4cf5e69fc
Julian <julian@corecode.io>
2022-09-12 22:36:48 +0200

    adjust ventura compilation macros

--- Mac/JMLoginItemManager.m
+++ Mac/JMLoginItemManager.m
@@ -68,7 +68,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 {
 	LOGFUNC
 
-#if defined(MAC_OS_X_VERSION_13_0) && defined(__MAC_OS_X_VERSION_MAX_ALLOWED) &&  __MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_13_0
+#if __MAC_OS_X_VERSION_MAX_ALLOWED >= __MAC_13_0
     if (@available(macOS 13.0, *))
     {
         return [[SMAppService mainAppService] status] == SMAppServiceStatusEnabled;
@@ -86,7 +86,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
     //[self willChangeValueForKey:@"launchesAtLogin"];
 
-#if defined(MAC_OS_X_VERSION_13_0) && defined(__MAC_OS_X_VERSION_MAX_ALLOWED) &&  __MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_13_0
+#if __MAC_OS_X_VERSION_MAX_ALLOWED >= __MAC_13_0
     if (@available(macOS 13.0, *))
     {
         NSError *error;
